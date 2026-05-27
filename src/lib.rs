57b07f0c3674e7b02b4c96eb562f4f19a3ef88e5
DexterHill0 <dreaminginsanity1@gmail.com>
2026-05-27 22:52:19 +0100

    feat: working win32 implementation

---
+++ src/lib.rs
@@ -0,0 +1,100 @@
+use std::{
+    cell::{Cell, RefCell},
+    collections::VecDeque,
+    rc::Rc,
+};
+
+use thiserror::Error;
+
+#[cfg(target_os = "windows")]
+pub mod win32;
+#[cfg(target_os = "windows")]
+use win32::TabletData;
+#[cfg(target_os = "windows")]
+pub use win32::WindowsError;
+
+#[derive(Error, Clone, Debug)]
+pub enum EasyTabError {
+    #[cfg(target_os = "windows")]
+    #[error("windows error: {0}")]
+    WindowsError(#[from] WindowsError),
+
+    #[cfg(feature = "raw-window-handle")]
+    #[error("{0}")]
+    HandleError(#[from] raw_window_handle::HandleError),
+
+    #[cfg(feature = "raw-window-handle")]
+    #[error("unsupported platform")]
+    UnsupportedPlatform,
+}
+
+pub type EasyTabResult<T> = std::result::Result<T, EasyTabError>;
+
+/// Initialisation options for the tablet.
+#[derive(Clone)]
+pub struct EasyTabOptions {
+    /// Divisor used to normalise raw pressure to 0.0..=1.0. Defaults to 1024.0.
+    pub pressure_normalization: f32,
+}
+
+impl Default for EasyTabOptions {
+    fn default() -> Self {
+        Self {
+            pressure_normalization: 1024.0,
+        }
+    }
+}
+
+#[non_exhaustive]
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+pub enum TabletEvent {
+    /// Pen entered hover range above the tablet.
+    StylusEnter,
+    /// Pen left hover range (or pointer capture was released).
+    StylusLeave,
+    /// Pen tip made contact with the tablet surface. `raw_pressure` is the un-normalised value.
+    StylusDown {
+        x: i32,
+        y: i32,
+        pressure: f32,
+        raw_pressure: f32,
+    },
+    /// Pen tip lifted from the tablet surface.
+    StylusUp { x: i32, y: i32 },
+    /// Pen moved while hovering or in contact. `pressure` is 0.0 when hovering.
+    StylusMove {
+        x: i32,
+        y: i32,
+        pressure: f32,
+        raw_pressure: f32,
+    },
+}
+
+#[doc(hidden)]
+pub(crate) struct TabletInner {
+    pub(crate) enabled: Cell<bool>,
+    pub(crate) events: RefCell<VecDeque<TabletEvent>>,
+    pub(crate) options: EasyTabOptions,
+}
+
+pub struct EasyTablet {
+    pub(crate) inner: Rc<TabletInner>,
+    pub(crate) data: TabletData,
+}
+
+#[cfg(feature = "raw-window-handle")]
+impl EasyTablet {
+    pub fn from_window<W: raw_window_handle::HasWindowHandle>(window: &W) -> EasyTabResult<Self> {
+        use raw_window_handle::RawWindowHandle;
+
+        let raw = window
+            .window_handle()
+            .map_err(EasyTabError::HandleError)?
+            .as_raw();
+
+        match raw {
+            RawWindowHandle::Win32(h) => Self::init(h.hwnd.get() as usize),
+            _ => Err(EasyTabError::UnsupportedPlatform),
+        }
+    }
+}
--- src/win32.rs
+++ src/win32.rs
@@ -1,441 +1,181 @@
-use std::cell::Cell;
+use std::cell::{Cell, RefCell};
+use std::collections::VecDeque;
 use std::rc::Rc;
 
-use windows::core::{implement, Error, IUnknown, InParam, Result, GUID, HRESULT};
-use windows::Win32::Foundation::{HANDLE_PTR, POINT};
-
-use windows::Win32::System::Com::{CoCreateInstance, CLSCTX_INPROC_SERVER};
-use windows::Win32::UI::TabletPC::{
-    IInkTablet, IRealTimeStylus, IStylusAsyncPlugin, IStylusAsyncPlugin_Impl, IStylusPlugin,
-    IStylusPlugin_Impl, RTSDI_AllData, RealTimeStylus, RealTimeStylusDataInterest, StylusInfo,
-    SYSTEM_EVENT_DATA,
+use thiserror::Error;
+use windows::Win32::Foundation::{HANDLE_PTR, HWND, LPARAM, LRESULT, WPARAM};
+use windows::Win32::System::Com::{COINIT_APARTMENTTHREADED, CoInitializeEx};
+use windows::Win32::UI::Input::Pointer::{GetPointerPenInfo, GetPointerType, POINTER_PEN_INFO};
+use windows::Win32::UI::Shell::{DefSubclassProc, RemoveWindowSubclass, SetWindowSubclass};
+use windows::Win32::UI::WindowsAndMessaging::{
+    POINTER_INPUT_TYPE, PT_PEN, WM_POINTERCAPTURECHANGED, WM_POINTERDOWN, WM_POINTERENTER,
+    WM_POINTERLEAVE, WM_POINTERUP, WM_POINTERUPDATE,
 };
 
-use crate::{EasyTabError, EasyTabOptions, EasyTabResult, EasyTablet, __InnerTablet};
+use crate::{EasyTabOptions, EasyTabResult, EasyTablet, TabletEvent, TabletInner};
 
-// ///
-// #[derive(Default, Clone, Copy, Debug)]
-// pub enum WinPropertyMetricUnit {
-//     #[default]
-//     Default = 0,
-//     Inches,
-//     Centimeters,
-//     Degrees,
-//     Radians,
-//     Seconds,
-//     Pounds,
-//     Grams,
+const SUBCLASS_ID: usize = 1;
 
-//     #[doc(hidden)]
-//     // final element used to get the number of variants in the enum
-//     __Final,
-// }
+#[derive(Debug, Error, Clone)]
+pub enum WindowsError {
+    #[error("failed to call CoInitializeEx ({0})")]
+    CoInitializeFailed(String),
 
-// impl From<TabletPropertyMetricUnit> for WinPropertyMetricUnit {
-//     fn from(tpm: TabletPropertyMetricUnit) -> Self {
-//         assert!(tpm.0 < WinPropertyMetricUnit::__Final as i32);
-//         // Safety:
-//         // Check performed above to make sure the inner value is not larger than the length of the enum.
-//         unsafe { std::mem::transmute(tpm.0 as i8) }
-//     }
-// }
-
-// ///
-// #[derive(Default, Clone, Copy, Debug)]
-// pub struct Property {
-//     min: i32,
-//     max: i32,
-
-//     units: TabletPropertyMetricUnit,
-//     resolution: f32,
-// }
-
-// ///
-// pub struct WinTab {}
-
-//
-#[derive(Default)]
-pub enum WinTabletIndex {
-    #[default]
-    Default,
-    Index(i32),
+    #[error("failed to set window subclass")]
+    FailedSubclass,
 }
 
-// ///
-// #[repr(u64)]
-// pub enum EasyTabProperty {
-//     X = TabletPC::GUID_PACKETPROPERTY_GUID_X.to_u128() as u64,
-//     Y,
-//     Z,
+pub struct TabletData {
+    hwnd: HWND,
+    subclass_data_ptr: usize,
+}
 
-//     PacketStatus,
-//     TimerTick,
-//     SerialNumber,
+struct HwndData {
+    tablet: Rc<TabletInner>,
+}
 
-//     NormalPressure,
-//     TangentPressure,
-//     ButtonPressure,
+fn cleanup_subclass(hwnd: HWND, subclass_data_ptr: usize) {
+    unsafe {
+        let _ = RemoveWindowSubclass(hwnd, Some(h_wndproc), SUBCLASS_ID);
 
-//     XTiltOrientation,
-//     YTiltOrientation,
-//     AzimuthOrientation,
-//     TwistOrientation,
-
-//     PitchRotation,
-//     RollRotation,
-//     YawRotation,
-
-//     Width,
-//     Height,
-
-//     FingerContantConfidence,
-//     ContactId,
-// }
-
-// function used to map a windows errors to an easytab error
-const ERROR_FN: fn(Error) -> EasyTabError = |e| EasyTabError::WinError(e.message());
+        drop(Box::from_raw(subclass_data_ptr as *mut HwndData));
+    }
+}
 
 impl EasyTablet {
-    /// Initialises a tablet.
-    ///
-    /// ## Arguments
-    ///
-    /// - `hwnd`: `HANDLE_PTR` - a handle to a window to bind the tablet to.
-    ///
-    /// <br>
-    ///
-    /// **Note**: This functions assumes that [`CoInitializeEx`](https://docs.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-coinitializeex) has previously been called.
-    /// - Refer to [`init_options`] for more info.
-
-    // pub fn init(hwnd: HANDLE_PTR) -> EasyTabResult<Self> {
-    //     EasyTablet::init_options(hwnd, EasyTabOptions::default())
-    // }
-
     pub fn init<W: Into<usize>>(hwnd: W) -> EasyTabResult<Self> {
-        EasyTablet::init_options(HANDLE_PTR(hwnd.into()), EasyTabOptions::default())
+        Self::init_options(HANDLE_PTR(hwnd.into()), EasyTabOptions::default())
     }
 
-    /// Initialises a tablet with the given options.
-    ///
-    /// ## Arguments
-    ///
-    /// - `hwnd`: `HANDLE_PTR`&emsp;&emsp;- a handle to a window to bind the tablet to.
-    /// - `opts`: `WinTabOptions` - the initialisation options for the tablet.
-    ///
-    /// <br>
-    ///
-    /// **Note**: This functions assumes that [`CoInitializeEx`](https://docs.microsoft.com/en-us/windows/win32/api/combaseapi/nf-combaseapi-coinitializeex) has previously been called.
-    ///
-    /// ```
-    /// // before calling `init_options`
-    /// unsafe {
-    ///     CoInitializeEx(
-    ///         std::ptr::null(),
-    ///         COINIT_APARTMENTTHREADED,
-    ///     )
-    ///     .expect("failed to initalise COM");
-    /// }
-    /// //...
-    /// let tablet = EasyTablet::init_options(...)?;
-    /// ```
-    pub fn init_options(hwnd: HANDLE_PTR, opts: EasyTabOptions) -> EasyTabResult<Self> {
-        // create a real time stylus
-        let stylus: IRealTimeStylus = unsafe {
-            CoCreateInstance(&RealTimeStylus, InParam::null(), CLSCTX_INPROC_SERVER)
-                .map_err(ERROR_FN)?
-        };
+    pub fn init_options(hwnd: HANDLE_PTR, options: EasyTabOptions) -> EasyTabResult<Self> {
+        let result = unsafe { CoInitializeEx(None, COINIT_APARTMENTTHREADED) };
 
-        // bind the stylus to the current window
-        unsafe { stylus.SetHWND(hwnd).map_err(ERROR_FN)? };
-
-        let slf = Self(Rc::new(__InnerTablet {
-            stylus,
-            opts,
-
-            on: Cell::default(),
-
-            active: Cell::default(),
-            x: Cell::default(),
-            y: Cell::default(),
-            pressure: Cell::default(),
-        }));
-
-        // pass a reference of ourselves into the handler so it can call the `handle_event` fn
-        let ash: IStylusAsyncPlugin = AsyncStylusHandler(Rc::clone(&slf.0)).into();
-
-        // add the handler to the stylus
-        unsafe {
-            slf.stylus
-                .AddStylusAsyncPlugin(
-                    slf.stylus.GetStylusAsyncPluginCount().map_err(ERROR_FN)?,
-                    &ash,
-                )
-                .map_err(ERROR_FN)?
-        };
-
-        Ok(slf)
-    }
-
-    /// Enables the tablet.
-    pub fn enable(&self) -> EasyTabResult<()> {
-        unsafe { self.stylus.SetEnabled(true).map_err(ERROR_FN)? };
-
-        Ok(())
-    }
-
-    /// Disables the tablet.
-    pub fn disable(&self) -> EasyTabResult<()> {
-        unsafe { self.stylus.SetEnabled(false).map_err(ERROR_FN)? };
-
-        Ok(())
-    }
-
-    pub fn on(&self, cb: Box<dyn Fn(WinTabEvent)>) {
-        self.on.set(Some(cb))
-    }
-
-    /// Returns whether a finger or stylus is activating the digitiser.
-    pub fn active(&self) -> bool {
-        self.active.get()
-    }
-
-    /// Returns the x position where the finger or stylus is making contact with the digitiser.
-    pub fn x(&self) -> i32 {
-        self.x.get()
-    }
-
-    /// Returns the y position where the finger or stylus is making contact with the digitiser.
-    pub fn y(&self) -> i32 {
-        self.y.get()
-    }
-
-    /// Returns the pressure of the finger or stylus on the digitiser.
-    pub fn pressure(&self) -> f32 {
-        self.pressure.get()
-    }
-}
-
-// TODO: Convert to trait?
-impl __InnerTablet {
-    // handles a stylus event
-    fn handle_event(&self, event: WinTabEvent) -> Result<()> {
-        match event {
-            WinTabEvent::StylusActive => self.active.set(true),
-            WinTabEvent::StylusInactive => self.active.set(false),
-
-            WinTabEvent::StylusButtonDown(x, y) | WinTabEvent::StylusButtonUp(x, y) => {
-                self.x.set(x);
-                self.y.set(y);
-            }
-            _ => todo!(),
+        if result.is_err() {
+            return Err(WindowsError::CoInitializeFailed(result.message()).into());
         }
 
-        // let on_ptr = self.on.
+        let hwnd_win = HWND(hwnd.0 as *mut _);
 
-        // if let Some(on) = on_ptr.into() {
-        //     let evfn = unsafe { on.as_ref().unwrap() };
+        let inner = Rc::new(TabletInner {
+            enabled: Cell::new(false),
+            events: RefCell::new(VecDeque::new()),
+            options,
+        });
 
-        // }
+        let ptr = Box::into_raw(Box::new(HwndData {
+            tablet: Rc::clone(&inner),
+        })) as usize;
 
-        // if let Some(on) = &self.on.get_mut() {
-        //     (*on)(event);
-        // }
+        let result = unsafe { SetWindowSubclass(hwnd_win, Some(h_wndproc), SUBCLASS_ID, ptr) };
 
-        Ok(())
+        if !result.as_bool() {
+            unsafe { drop(Box::from_raw(ptr as *mut HwndData)) };
+
+            return Err(WindowsError::FailedSubclass.into());
+        }
+
+        Ok(Self {
+            inner,
+            data: TabletData {
+                hwnd: hwnd_win,
+                subclass_data_ptr: ptr,
+            },
+        })
+    }
+
+    pub fn enable(&self) {
+        self.inner.enabled.set(true);
+    }
+
+    pub fn disable(&self) {
+        self.inner.enabled.set(false);
+    }
+
+    /// Drains all pending tablet events. Call this once per frame.
+    pub fn poll_events(&self) -> impl Iterator<Item = TabletEvent> {
+        std::mem::take(&mut *self.inner.events.borrow_mut()).into_iter()
     }
 }
 
-#[derive(Debug)]
-pub enum WinTabEvent {
-    StylusActive,
-    StylusInactive,
-    //...............x....y
-    StylusButtonDown(i32, i32),
-    StylusButtonUp(i32, i32),
-}
-
-// the plugin added to the real time stylus to allow getting real time events from the stylus (asynchronously)
-#[implement(IStylusAsyncPlugin)]
-struct AsyncStylusHandler(Rc<__InnerTablet>);
-
-impl IStylusPlugin_Impl for AsyncStylusHandler {
-    fn RealTimeStylusEnabled(
-        &self,
-        _: &Option<IRealTimeStylus>,
-        _: u32,
-        _: *const u32,
-    ) -> Result<()> {
-        Ok(())
-    }
-
-    fn RealTimeStylusDisabled(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        _: u32,
-        _: *const u32,
-    ) -> Result<()> {
-        debug_assert!(pirtssrc.as_ref().unwrap() == &self.0.as_ref().stylus);
-
-        self.0.as_ref().handle_event(WinTabEvent::StylusInactive)
-    }
-
-    fn StylusInRange(&self, _: &Option<IRealTimeStylus>, _: u32, _: u32) -> Result<()> {
-        Ok(())
-    }
-
-    fn StylusOutOfRange(&self, pirtssrc: &Option<IRealTimeStylus>, _: u32, _: u32) -> Result<()> {
-        debug_assert!(pirtssrc.as_ref().unwrap() == &self.0.as_ref().stylus);
-
-        self.0.as_ref().handle_event(WinTabEvent::StylusInactive)
-    }
-
-    fn StylusDown(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        _: *const StylusInfo,
-        _: u32,
-        _: *const i32,
-        _: *mut *mut i32,
-    ) -> Result<()> {
-        // checking that the stylus receiving events is the same stylus the tablet is pointing to
-        debug_assert!(pirtssrc.as_ref().unwrap() == &self.0.as_ref().stylus);
-
-        self.0.as_ref().handle_event(WinTabEvent::StylusActive)
-    }
-
-    fn StylusUp(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        _: *const StylusInfo,
-        _: u32,
-        _: *const i32,
-        _: *mut *mut i32,
-    ) -> Result<()> {
-        debug_assert!(pirtssrc.as_ref().unwrap() == &self.0.as_ref().stylus);
-
-        self.0.as_ref().handle_event(WinTabEvent::StylusInactive)
-    }
-
-    // TODO: test with more tablets - with my tablet, the GUID for the button doesnt seem to be any real, registered COM class.
-    fn StylusButtonDown(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        _: u32,
-        _: *const GUID, // what is this?
-        pstyluspos: *mut POINT,
-    ) -> Result<()> {
-        debug_assert!(pirtssrc.as_ref().unwrap() == &self.0.as_ref().stylus);
-
-        let point = unsafe { &*pstyluspos };
-
-        self.0
-            .as_ref()
-            .handle_event(WinTabEvent::StylusButtonDown(point.x, point.y))
-    }
-
-    fn StylusButtonUp(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        _: u32,
-        pguidstylusbutton: *const GUID,
-        pstyluspos: *mut POINT,
-    ) -> Result<()> {
-        debug_assert!(pirtssrc.as_ref().unwrap() == &self.0.as_ref().stylus);
-
-        let point = unsafe { &*pstyluspos };
-
-        self.0
-            .as_ref()
-            .handle_event(WinTabEvent::StylusButtonUp(point.x, point.y))
-    }
-
-    fn InAirPackets(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        pstylusinfo: *const StylusInfo,
-        cpktcount: u32,
-        cpktbufflength: u32,
-        ppackets: *const i32,
-        pcinoutpkts: *mut u32,
-        ppinoutpkts: *mut *mut i32,
-    ) -> Result<()> {
-        //println!("InAirPackets");
-        Ok(())
-    }
-
-    fn Packets(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        pstylusinfo: *const StylusInfo,
-        cpktcount: u32,
-        cpktbufflength: u32,
-        ppackets: *const i32,
-        pcinoutpkts: *mut u32,
-        ppinoutpkts: *mut *mut i32,
-    ) -> Result<()> {
-        println!("Packets");
-        Ok(())
-    }
-
-    fn CustomStylusDataAdded(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        pguidid: *const GUID,
-        cbdata: u32,
-        pbdata: *const u8,
-    ) -> Result<()> {
-        println!("CustomStylusDataAdded");
-        Ok(())
-    }
-
-    fn SystemEvent(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        tcid: u32,
-        sid: u32,
-        event: u16,
-        eventdata: &SYSTEM_EVENT_DATA,
-    ) -> Result<()> {
-        println!("SystemEvent");
-        Ok(())
-    }
-
-    fn TabletAdded(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        pitablet: &Option<IInkTablet>,
-    ) -> Result<()> {
-        println!("TabletAdded");
-        Ok(())
-    }
-
-    fn TabletRemoved(&self, pirtssrc: &Option<IRealTimeStylus>, itabletindex: i32) -> Result<()> {
-        println!("TabletRemoved");
-        Ok(())
-    }
-
-    fn Error(
-        &self,
-        pirtssrc: &Option<IRealTimeStylus>,
-        piplugin: &Option<IStylusPlugin>,
-        datainterest: RealTimeStylusDataInterest,
-        hrerrorcode: HRESULT,
-        lptrkey: *mut isize,
-    ) -> Result<()> {
-        println!("Error");
-        Ok(())
-    }
-
-    fn UpdateMapping(&self, pirtssrc: &Option<IRealTimeStylus>) -> Result<()> {
-        println!("UpdateMapping");
-        Ok(())
-    }
-
-    fn DataInterest(&self) -> Result<RealTimeStylusDataInterest> {
-        // collect all data on stylus events
-        Ok(RTSDI_AllData)
+#[cfg(target_os = "windows")]
+impl Drop for EasyTablet {
+    fn drop(&mut self) {
+        cleanup_subclass(self.data.hwnd, self.data.subclass_data_ptr);
     }
 }
 
-// `IStylusAsyncPlugin_Impl` is created by the `implement` macro
-impl IStylusAsyncPlugin_Impl for AsyncStylusHandler {}
+const fn loword(l: usize) -> u32 {
+    (l & 0xffff) as u32
+}
+
+unsafe extern "system" fn h_wndproc(
+    hwnd: HWND,
+    umsg: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+    _uidsubclass: usize,
+    dwrefdata: usize,
+) -> LRESULT {
+    let data = unsafe { &*(dwrefdata as *const HwndData) };
+
+    if !data.tablet.enabled.get() {
+        return unsafe { DefSubclassProc(hwnd, umsg, wparam, lparam) };
+    }
+
+    'outer: {
+        match umsg {
+            WM_POINTERENTER
+            | WM_POINTERLEAVE
+            | WM_POINTERCAPTURECHANGED
+            | WM_POINTERDOWN
+            | WM_POINTERUP
+            | WM_POINTERUPDATE => {
+                let pointer_id = loword(wparam.0);
+
+                let mut pointer_type = POINTER_INPUT_TYPE(0);
+
+                if unsafe { GetPointerType(pointer_id, &mut pointer_type).is_err() }
+                    || pointer_type != PT_PEN
+                {
+                    break 'outer;
+                }
+
+                let mut pen_info: POINTER_PEN_INFO = unsafe { std::mem::zeroed() };
+
+                if unsafe { GetPointerPenInfo(pointer_id, &mut pen_info).is_err() } {
+                    break 'outer;
+                }
+
+                let x = pen_info.pointerInfo.ptPixelLocation.x;
+                let y = pen_info.pointerInfo.ptPixelLocation.y;
+
+                let raw_pressure = pen_info.pressure as f32;
+                let pressure = raw_pressure / data.tablet.options.pressure_normalization;
+
+                let event = match umsg {
+                    WM_POINTERENTER => TabletEvent::StylusEnter,
+                    WM_POINTERLEAVE | WM_POINTERCAPTURECHANGED => TabletEvent::StylusLeave,
+                    WM_POINTERDOWN => TabletEvent::StylusDown {
+                        x,
+                        y,
+                        pressure,
+                        raw_pressure,
+                    },
+                    WM_POINTERUP => TabletEvent::StylusUp { x, y },
+                    _ => TabletEvent::StylusMove {
+                        x,
+                        y,
+                        pressure,
+                        raw_pressure,
+                    },
+                };
+
+                data.tablet.events.borrow_mut().push_back(event);
+            },
+
+            _ => {},
+        }
+    }
+
+    unsafe { DefSubclassProc(hwnd, umsg, wparam, lparam) }
+}
